332b653bf3ac42c0445a78e7c5c109ef78664256
VenturaGiu <giulia-ratti@hotmail.com>
2021-06-25 18:29:47 -0300

    Demitido

--- config/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const secret = '9bIopuq2ea5gDTspoDzQHTdXFTZJgQFfsNKY';
-
-module.exports = {
-    sign: payload => jwt.sign(payload, secret, {expiresIn: 86400}),
-    verify:  token => jwt.verify(token, secret)
-}
